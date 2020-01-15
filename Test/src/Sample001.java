0735879a41a74a4f11b4d57f49a984c192e64968
junpei-oba <0728jp15@gmail.com>
2020-01-15 20:18:54 +0900

    first commit

---
+++ Test/src/Sample001.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+/**
+ * @author 淳平
+ *
+ */
+public class Sample001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+	}
+
+}
